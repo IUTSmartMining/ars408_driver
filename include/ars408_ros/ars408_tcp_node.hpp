451d4dd7f7d5f6ee014926be73d0edbfc3c05e28
Mohammadreza Mirdamadiyan <smrm6789@gmail.com>
2024-05-21 11:52:20 +0330

    add tcp node

---
+++ include/ars408_ros/ars408_tcp_node.hpp
@@ -0,0 +1,37 @@
+// Copyright 2021 Perception Engine, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef ARS408_ROS__ARS408_TCP_NODE_HPP_
+#define ARS408_ROS__ARS408_TCP_NODE_HPP_
+
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include "ars408_ros/ars408_tcp_server.hpp"
+
+namespace ars408
+{
+    class TcpServerNode : public rclcpp::Node
+    {
+    public:
+        TcpServerNode();
+
+    private:
+        void publish_data(const std::string& data);
+
+        TcpServer tcp_server_;
+        rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
+    };
+}
+
+#endif  // ARS408_ROS__ARS408_TCP_NODE_HPP_
---
+++ src/ars408_tcp_node.cpp
@@ -0,0 +1,34 @@
+// Copyright 2021 Perception Engine, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "ars408_ros/ars408_tcp_node.hpp"
+
+namespace ars408
+{
+    TcpServerNode::TcpServerNode()
+        : Node("ars_tcp_server_node"), tcp_server_(12345)
+    {
+        publisher_ = this->create_publisher<std_msgs::msg::String>("tcp_data", 10);
+        tcp_server_.set_data_callback([this](const std::string& data) {
+            this->publish_data(data);
+            });
+        tcp_server_.start();
+    }
+
+    void TcpServerNode::publish_data(const std::string& data) {
+        auto message = std_msgs::msg::String();
+        message.data = data;
+        publisher_->publish(message);
+    }
+}
